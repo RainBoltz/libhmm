6cf0cb08d7252dcf09857d987b2c7818e92aa8d0
Philip Wang <RainBoltz@users.noreply.github.com>
2019-05-29 13:53:59 +0800

    first commit

---
+++ Libhmm/Libhmm.cpp
@@ -0,0 +1,51 @@
+﻿// Libhmm.cpp : 定義 DLL 應用程式的匯出函式。
+//
+
+#include "stdafx.h"
+#include <random>
+#include "Libhmm.h"
+
+namespace Libhmm {
+	HMM::HMM(int n, int m): N(n), M(m){
+		init_prob(A, n, n);
+		init_prob(B, n, m);
+		init_prob(pi, n);
+	}
+	HMM::HMM(char* model_path){
+		load_model(model_path);
+	}
+	HMM::~HMM() {
+		free_memory(A);
+		free_memory(B);
+		free_memory(pi);
+	};
+	void init_prob(double* M, int i) {
+		M = new double[i];
+		for (int c = 0; c < i; c++)
+			M[c] = 1.0 / i;
+	}
+	void init_prob(double** M, int i, int j) {
+		for (int a = 0; a < i; i++) {
+			M = new double*[i];
+			for (int b = 0; b < j; b++) {
+				M[b] = new double[j];
+				for (int c = 0; c < j; c++)
+					M[b][c] = 1.0 / j;
+			}
+		}
+	}
+	void free_memory(double* M) {
+		delete[] M;
+	}
+	void free_memory(double** M) {
+		int _n = _msize(M[0]) / sizeof(M[0][0]);
+		for (int i = _n - 1; i >= 0; i++)
+			delete[] M[i];
+	}
+	void HMM::load_model(char* model_path){
+
+	}
+	void HMM::dump_model(char* output_path){
+
+	}
+}
---
+++ Libhmm/Libhmm.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+namespace Libhmm
+{
+	public ref class HMM
+	{
+	public:
+		HMM(int n, int m);
+		HMM(char* model_path);
+		~HMM();
+		void load_model(char* model_path);
+		void dump_model(char* output_path);
+
+	private:
+		int N;		/* number of hidden states;  Q={1,2,...,N}*/
+		int M;		/* number of observation symbols; V={1,2,...,M}*/
+		double** A;	/* A[1..N][1..N]. a[i][j] is the transition prob of going from state i at time t to state j at time t+1*/
+		double** B;	/* B[1..N][1..M]. b[j][k] is the probability of of observing symbol k in state j*/
+		double* pi;	/* pi[1..N] pi[i] is the initial state distribution*/
+		void init_prob(double* M, int i);
+		void init_prob(double** M, int i, int j);
+		void free_memory(double* M);
+		void free_memory(double** M);
+	};
+
+	public ref class HMM_Trainer
+	{
+	public:
+		HMM_Trainer(int n, int m, int t);
+		~HMM_Trainer();
+		double **alpha[T][N], **beta[T][N];        /* evaluation problem*/
+		double **delta[T][N]; int **phi[T][N];    /* decoding problem*/
+		double **gamma[T][N], ***xi[T][N][N];     /* learning problem*/
+	};
+
+}
